ac979ea56c556e2f878be08767c40f6d0d6ee790
r-brt <Robertgb88@gmail.com>
2026-03-12 14:25:06 -0400

    Add updateinventory to dashboard

--- index.php
+++ index.php
@@ -610,6 +610,17 @@
         <button class="arrow-button">→</button>
     </div>
 
+    <!-- Update Inventory -->
+    <div class="content-box-test" onclick="window.location.href='viewUpdateInventory.php'" style="background-color: #4d98f3; border-radius: 12px; padding: 20px; color: white;">
+        <div class="icon-overlay">
+            <img style="border-radius: 5px;" src="images/list-solid.svg" alt="Update Inventory Icon">
+        </div>
+        
+        <div class="large-text-sub" style="color:white;">Update Inventory</div>
+        <div class="graph-text" style="color:#3A3A3A;">Submit new item counts.</div>
+        <button class="arrow-button">→</button>
+    </div>
+
     <!-- View Drafts -->
     <!-- <div class="content-box-test" onclick="window.location.href='viewDrafts.php'" style="background-color: #4d98f3; border-radius: 12px; padding: 20px; color: white;">
         <div class="icon-overlay">
