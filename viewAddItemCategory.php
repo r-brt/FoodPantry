2dc7bc686198697dd00fbbbe11cbd9fed944a5f2
r-brt <Robertgb88@gmail.com>
2026-04-24 22:29:02 -0400

    Adding Deleted Category updates info

--- viewAddItemCategory.php
+++ viewAddItemCategory.php
@@ -22,10 +22,14 @@
             $bananaBox = isset($_POST['bananaBox']) ? 1 : 0;
             $itemsPerBox = intval($_POST['itemsPerBox']);
             $status = "Active";
-
-            if (retrieve_ItemCategory_by_name($cat_name)) {
-                if(retrieve_ItemCategoryStatus($cat_name) == 'Deleted') {
-                    activate_itemCategory(retrieve_ItemID($cat_name));
+            
+            $existing_item = retrieve_ItemCategory_by_name($cat_name);
+            if ($existing_item) {
+                if($existing_item->getStatus() == 'Deleted') {
+                    activate_itemCategory($existing_item->getId());
+                    update_itemCategory($existing_item->getId(), $cat_name, $bananaBox, $itemsPerBox);
+                    header("Location: viewItemCategories.php");
+                    exit();
                 } else {
                     $errors[] = "Category already exists";
                 }
